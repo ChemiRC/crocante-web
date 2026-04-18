cd81667023c9b8a56515e5bf92103b1230921fd7
José Miguel Ramírez Cano <josemy2020@gmail.com>
2026-04-17 18:25:07 -0600

    Forzar actualización de estilos de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
         'crocante-gold': '#d4af37',
-        'crocante-dark': '#2c2c2c', // Para textos legibles
+        'crocante-dark': '#2c2c2c',
         'marble-white': '#fdfdfd',
       },
       backgroundImage: {
-        // Este es el "truco" para el efecto mármol sutil
         'marble-pattern': "radial-gradient(circle at 50% 50%, rgba(200, 200, 200, 0.05) 0%, transparent 80%), linear-gradient(120deg, #fdfdfd 0%, #f5f5f5 100%)",
       }
     },
